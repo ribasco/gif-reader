a39300ced863bf9569cebf5c887b60ba1e04119f
Rafael Ibasco <ribasco@gmail.com>
2021-01-05 15:07:56 +0800

    Updated exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module com.ibasco.gifreader {
+    exports com.ibasco.image.gif;
+    exports com.ibasco.image.gif.enums;
+    exports com.ibasco.image.gif.exceptions;
+    exports com.ibasco.image.gif.util;
+
     requires org.slf4j;
     requires java.desktop;
     requires org.apache.commons.compress;
